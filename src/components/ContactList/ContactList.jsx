e44ac93dd95c3d4a380a15e0fb62514eccd719fe
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-07 00:00:59 +0300

    add selector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,20 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "../ContactList/ContactList.module.css";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 export default function ContactList() {
-  const items = useSelector(selectContacts);
-  const nameFilter = useSelector(selectNameFilter);
-
-  const filteredContacts = items.filter((item) =>
-    item.name.toLowerCase().includes(nameFilter)
-  );
+  const items = useSelector(selectFilteredContacts);
 
   return (
-    <ul className={css.menuList}>
-      {filteredContacts.map((item) => (
-        <li className={css.item} key={item.id}>
-          <Contact item={item} />
-        </li>
-      ))}
-    </ul>
+    <div className={css.wrap}>
+      <ul className={css.menuList}>
+        {items.map((item) => (
+          <li className={css.item} key={item.id}>
+            <Contact item={item} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
